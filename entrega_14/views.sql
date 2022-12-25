aa8380e8db14b5f132b149263e7fd986c929c067
carlos8788 <carlos8788@gmail.com>
2022-12-25 12:57:42 -0300

    entrega_14

--- entrega_14/views.sql
+++ entrega_14/views.sql
@@ -1,6 +1,6 @@
-#Script de creación de vistas
-# Luis Carlos Pérez Gonzalez 
-
+-- Script de creación de vistas
+-- Luis Carlos Pérez Gonzalez 
+use vuelos_comerciales;
 -- Vista que muestra que pilotos no tienen vuelos designados
 CREATE OR REPLACE VIEW piloto_sin_vuelos AS
 SELECT DISTINCT id_piloto, nombre_piloto, apellido_piloto 
@@ -27,4 +27,25 @@ SELECT horario_vuelo, fecha,
 	FROM piloto
     WHERE vuelo.id_piloto = piloto.id_piloto) piloto
 FROM vuelo;
+
+-- Vista que muestra el nombre de los pasajes que fueron comprados
+CREATE OR REPLACE VIEW detalle_pasaje 
+AS
+SELECT 
+asiento,
+	(SELECT DISTINCT CONCAT(nombre_pasajero, " ", apellido_pasajero)
+    FROM pasajero
+    INNER JOIN vuelo ON pasaje.id_pasajero=pasajero.id_pasajero) pasajero,
+    id_vuelo
+FROM pasaje;
+
+-- Vista de aviones que no tienen vuelo programado
+
+CREATE OR REPLACE VIEW aviones_sin_vuelo
+AS
+SELECT DISTINCT id_avion, modelo
+FROM avion
+WHERE id_avion NOT IN (SELECT id_avion
+							FROM vuelo);
+
 
